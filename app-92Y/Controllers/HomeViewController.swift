5a6eeb4bd5a55a99f6d0cdbe9123e374e0ff49f3
Randy Camacho <43118312+RandyCamacho@users.noreply.github.com>
2020-11-14 21:01:09 -0700

    starter files

---
+++ app-92Y/Controllers/HomeViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  app-92Y
+//
+//  Created by Randy Camacho on 11/14/20.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .black
+        // Do any additional setup after loading the view.
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        let isLoggedIn = UserDefaults.standard.bool(forKey: "loggen_in")
+        
+        if !isLoggedIn {
+            let vc = LoginViewController()
+            let nav = UINavigationController(rootViewController: vc)
+            nav.modalPresentationStyle = .fullScreen
+            present(nav, animated: true)
+        }
+    }
+    
+    
+    
+    
+
+
+}
+
